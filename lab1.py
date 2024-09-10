4d14cc3d7c6522e9482186b82ba13d41853c14f9
Daedma <hdamir163@gmail.com>
2024-09-10 14:50:17 +0400

    Add support for command line arguments

--- lab1.py
+++ lab1.py
@@ -1,5 +1,6 @@
 import math
 import re
+import sys
 
 def create_y(a:float, b:float, c:float) -> float:
     return lambda x : a * (math.sin(b * x) - (math.cos(c * x))**2)
@@ -21,7 +22,16 @@ def calc_in_range(func, lower, upper, step):
         x += step
     return values
 
-params = load_params("config")
+params = dict()
+if len(sys.argv) > 1:
+    params['n0'] = float(sys.argv[1])
+    params['h'] = float(sys.argv[2])
+    params['nk'] = float(sys.argv[3])
+    params['a'] = float(sys.argv[4])
+    params['b'] = float(sys.argv[5])
+    params['c'] = float(sys.argv[6])
+else:
+    params = load_params("config")
 y = create_y(params['a'], params['b'], params['c'])
 values = calc_in_range(y, params['n0'], params['nk'], params['h'])
 with open("result", 'w') as file:
