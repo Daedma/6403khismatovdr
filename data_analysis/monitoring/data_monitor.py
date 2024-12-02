1ec65f73ae4efc890bddf74169cbb03bdcc9c59f
Daedma <hdamir163@gmail.com>
2024-12-03 00:38:58 +0400

    Добавил документацию + аннотации

--- data_analysis/monitoring/data_monitor.py
+++ data_analysis/monitoring/data_monitor.py
@@ -11,12 +11,18 @@ class CancellationToken:
     """
 
     def __init__(self):
+        """
+        Инициализация токена отмены.
+        """
         self.__is_canceled = False
 
     def is_canceled(self) -> bool:
         """
         Метод проверки отмены задачи.
-        Returns:
+
+        Возвращает:
+        ----------
+        bool
             True, если задача была отменена.
         """
         return self.__is_canceled
@@ -29,15 +35,52 @@ class CancellationToken:
 
 
 class DataMonitor:
+    """
+    Класс для мониторинга данных и записи их в файл.
+
+    Атрибуты:
+    ---------
+    __cancellation_token : CancellationToken
+        Токен отмены выполнения задачи.
+    __data_source : Any
+        Источник данных для мониторинга.
+    __thread : threading.Thread
+        Поток выполнения задачи мониторинга.
+    __logger : logging.Logger
+        Логгер для записи сообщений.
+    __data : np.array
+        Массив данных для мониторинга.
+    """
 
     def __init__(self, data_source):
+        """
+        Инициализация объекта DataMonitor.
+
+        Параметры:
+        ----------
+        data_source : Any
+            Источник данных для мониторинга.
+        """
         self.__cancellation_token = None
         self.__data_source = data_source
         self.__thread = None
         self.__logger = logging.getLogger(__name__)
         self.__data = np.array([])
 
-    def run(self, output_file="out.txt"):
+    def run(self, output_file: str = "out.txt") -> None:
+        """
+        Запуск сервиса мониторинга данных.
+
+        Параметры:
+        ----------
+        output_file : str, optional
+            Файл для записи результатов мониторинга (по умолчанию "out.txt").
+
+        Исключения:
+        -----------
+        RuntimeError
+            Если сервис уже запущен.
+        """
         self.__prerun()
         if self.__thread is not None:
             error = RuntimeError("Попытка повторного запуска сервиса")
@@ -55,9 +98,14 @@ class DataMonitor:
             self.__logger.error(error)
             raise error
 
-    def stop(self):
+    def stop(self) -> None:
         """
         Метод остановки сервиса.
+
+        Исключения:
+        -----------
+        RuntimeError
+            Если сервис не был запущен.
         """
         if self.__thread is None:
             error = RuntimeError("Попытка остановить незапущенный сервис")
@@ -68,7 +116,19 @@ class DataMonitor:
         self.__thread = None
         self.__logger.info("Сервис завершил работу")
 
-    def __run_Impl(self, output_file, cancellation_token):
+    def __run_Impl(
+        self, output_file: str, cancellation_token: CancellationToken
+    ) -> None:
+        """
+        Внутренний метод выполнения задачи мониторинга.
+
+        Параметры:
+        ----------
+        output_file : str
+            Файл для записи результатов мониторинга.
+        cancellation_token : CancellationToken
+            Токен отмены выполнения задачи.
+        """
         while not cancellation_token.is_canceled():
             value = self.__data_source.get_current()
             self.__data = np.append(self.__data, value)
@@ -83,7 +143,15 @@ class DataMonitor:
             self.__logger.info(f"Данные записаны в файл: {output_file}")
             time.sleep(5)
 
-    def __prerun(self):
+    def __prerun(self) -> None:
+        """
+        Метод предварительной загрузки данных перед запуском сервиса.
+
+        Исключения:
+        -----------
+        RuntimeError
+            Если произошла ошибка при загрузке данных.
+        """
         self.__logger.info("Начало загрузки данных за последний день")
         try:
             self.__data = self.__data_source.get_period(period="1d", interval="1m")
--- data_analysis/zfinance/finance_data.py
+++ data_analysis/zfinance/finance_data.py
@@ -2,14 +2,54 @@ import yfinance as yf
 
 
 class FinanceData:
-    def __init__(self, ticker):
+    """
+    Класс для получения финансовых данных с использованием библиотеки yfinance.
+
+    Атрибуты:
+    ---------
+    _ticker : str
+        Тикер акции для получения финансовых данных.
+    """
+
+    def __init__(self, ticker: str):
+        """
+        Инициализация объекта FinanceData.
+
+        Параметры:
+        ----------
+        ticker : str
+            Тикер акции для получения финансовых данных.
+        """
         self._ticker = ticker
 
-    def get_current(self):
+    def get_current(self) -> float:
+        """
+        Получает текущую цену закрытия акции.
+
+        Возвращает:
+        ----------
+        float
+            Текущая цена закрытия акции.
+        """
         stock = yf.Ticker(self._ticker)
         current_price = stock.history(period="1d")["Close"][0]
         return current_price
 
-    def get_period(self, period="1mo", interval="1d"):
+    def get_period(self, period: str = "1mo", interval: str = "1d") -> list:
+        """
+        Получает исторические данные цен закрытия акции за указанный период и интервал.
+
+        Параметры:
+        ----------
+        period : str, optional
+            Период для получения данных (по умолчанию "1mo" - один месяц).
+        interval : str, optional
+            Интервал для получения данных (по умолчанию "1d" - один день).
+
+        Возвращает:
+        ----------
+        list
+            Список исторических цен закрытия акции за указанный период и интервал.
+        """
         data = yf.download(self._ticker, period=period, interval=interval)
         return data["Close"].values
